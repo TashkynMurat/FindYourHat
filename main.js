bcc1ee1f514932762e9e48662ab29354550388d2
Tashkyn Murat <tashkynmurat@gmail.com>
2021-02-08 10:15:57 +0600

    Fix problems

--- main.js
+++ main.js
@@ -14,76 +14,71 @@ class Field {
     }
 
     Game() {
-        let gameInProgress = true;
-        while(gameInProgress) {
-            this.print();
-            this.question();
-            if (this.isHat()) {
-                console.log('You won!');
-                gameInProgress = false;
-                break;
-            } else if (this.isHole) {
-                console.log('You fell in hole!');
-                gameInProgress = false;
-                break;
-            } else if (!(this.Boundaries)) {
-                console.log('You are out of boundaries!');
-                gameInProgress = false;
-                break;
-            }
-
-            this.field[this.locationY][this.locationX] = pathCharacter;
+        let playing = true;
+        while (playing) {
+          this.print();
+          this.askQuestion();
+          if (!this.isInBounds()) {
+            console.log('You are out of boundaries!');
+            playing = false;
+            break;
+          } else if (this.isHole()) {
+            console.log('You fell into the hole');
+            playing = false;
+            break;
+          } else if (this.isHat()) {
+            console.log('You won!');
+            playing = false;
+            break;
+          }
+          // Update the current location on the map
+          this.field[this.locationY][this.locationX] = pathCharacter;
         }
-    }
-
+      }
+    
+      askQuestion() {
+        const answer = prompt('Which way? ').toUpperCase();
+        switch (answer) {
+          case 'U':
+            this.locationY -= 1;
+            break;
+          case 'D':
+            this.locationY += 1;
+            break;
+          case 'L':
+            this.locationX -= 1;
+            break;
+          case 'R':
+            this.locationX += 1;
+            break;
+          default:
+            console.log('Enter U, D, L or R.');
+            this.askQuestion();
+            break;
+        }
+      }
+    
+      isInBounds() {
+        return (
+          this.locationY >= 0 &&
+          this.locationX >= 0 &&
+          this.locationY < this.field.length &&
+          this.locationX < this.field[0].length
+        );
+      }
+    
+      isHat() {
+        return this.field[this.locationY][this.locationX] === hat;
+      }
+    
+      isHole() {
+        return this.field[this.locationY][this.locationX] === hole;
+      }
+        
+    
     print() {
         for(let i = 0; i < this.field.length; i++) {
             console.log(this.field[i].join(''));
         }
     }
-
-    question() {
-        const input = prompt('Where? (U, D, L, R)').toUpperCase();
-        switch (input) {
-            case 'U':
-                this.locationY -= 1;
-                break;
-            case 'D':
-                this.locationY += 1;
-                break;
-            case 'L':
-                this.locationX -= 1;
-                break;
-            case 'R':
-                this.locationX += 1;
-            default:
-                console.log('You enetered invalid value! Try again! (U - up, D - down, L - left, R - right)');
-                this.question();
-                break;
-
-        }
-    }
-
-    isHole() {
-        return this.field[this.locationY][this.locationX] === hole;
-    }
-
-    isHat() {
-        return this.field[this.locationY][this.locationX] === hat;
-    }
-
-    Boundaries() {
-        return (
-            this.locationY >= 0 &&
-            this.locationX >= 0 &&
-            this.locationY < this.field.length &&
-            this.locationX < this.field[0].length
-          );
-    }
 }
-
-const test = new Field([
-    ['*', '░', '0'],
-    ['░', '0', '░'],
-    ['░', '^', '░']]);
-test.Game();
