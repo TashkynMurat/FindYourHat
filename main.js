aad9244bb8b7f855d3b0ea97b6797fc31502ba83
Tashkyn Murat <tashkynmurat@gmail.com>
2021-02-08 08:18:31 +0600

    Add print function

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field) {
+        this._field = field;
+    }
+
+    get field() {
+        return this._field;
+    }
+
+    print() {
+        for(let i = 0; i < this.field.length; i++) {
+            console.log(this.field[i].join(''));
+        }
+    }
+}
+
+const test = new Field([
+    ['*', '░', '0'],
+    ['░', '0', '░'],
+    ['░', '^', '░']]);
+test.print();
